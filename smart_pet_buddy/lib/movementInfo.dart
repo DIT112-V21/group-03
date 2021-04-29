e4d8344fbb097e1bf6f59510ef715577a947d6dd
Emilie Anthony <emilieanthony@live.se>
2021-04-29 09:39:16 +0200

    Add movementInfo class and movementWidget class. Update custompage.dart with some updated UI and refactor code.
    Co-authored-by: InaJ123 <81151459+inaj123@users.noreply.github.com>
    Co-authored-by: olga <gusratol@student.gu.se>
    Co-authored-by: emilieanthony <59511737+emilieanthony@users.noreply.github.com>
    Co-authored-by: flyf <hry87625@gmail.com>
    Co-authored-by: AstridBer <gusbernas@student.gu.se>
    Co-authored-by: sofie-a <annsofiexeriksson@gmail.com>

---
+++ smart_pet_buddy/lib/movementInfo.dart
@@ -0,0 +1,16 @@
+class MovementInfo {
+  String title;
+  String imageUrl;
+  String command;
+
+  MovementInfo(
+      String title,
+      String imageUrl,
+      String command,
+      )
+  {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.command = command;
+  }
+}
