b585bdbdea9034cfc1c182b9c3671758a156d5b5
Ann-Sofie <annsofiexeriksson@gmail.com>
2021-04-30 09:31:23 +0200

    Edit User Profile UI

---
+++ smart_pet_buddy/lib/settingspage.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class SettingsPage extends StatefulWidget {
+  @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: Text('Profile'),
+        backgroundColor:Colors.green.shade400,
+      ),
+      body: Center(
+        child: Text('Soon Profile'),
+      ),
+    );
+  }
+}
