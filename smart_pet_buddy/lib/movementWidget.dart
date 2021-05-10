1977d0de71d21755a1b7a6f01c75f10d3226af28
Emilie Anthony <emilieanthony@live.se>
2021-05-10 18:00:19 +0200

    add play and pause icons

--- smart_pet_buddy/lib/movementWidget.dart
+++ smart_pet_buddy/lib/movementWidget.dart
@@ -21,30 +21,29 @@ class _MovementWidgetState extends State<MovementWidget> {
         padding: const EdgeInsets.all(0),
         decoration: BoxDecoration(
           image: DecorationImage(
+            colorFilter: new ColorFilter.mode(Colors.black.withOpacity(0.5), BlendMode.softLight),
             image: NetworkImage(widget.info.imageUrl),
             fit: BoxFit.cover,
           ),
           borderRadius: BorderRadius.circular(12),
         ),
+
         child: TextButton(
           onPressed: widget.commandRunning == "" || widget.commandRunning == widget.info.command ? widget.onPressed : null, //null means that nothing happens when on pressed (button disabled)
-          child: Ink(
-            decoration: BoxDecoration(
-              /*gradient: LinearGradient(
-                colors: [
-                  Colors.black,
-                  Colors.transparent
-                ],
-                begin: Alignment.bottomCenter,
-                end: Alignment.topCenter,
-              ),*/
-            ),
-            child: Align(
+          child: Align(
               alignment: Alignment.bottomLeft,
-              child: Text(widget.commandRunning == widget.info.command ? "Busy driving..." : widget.info.title, textAlign: TextAlign.left, style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 25),
-              ),
-            ),
-          ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(widget.commandRunning == widget.info.command ? "Busy driving..." : widget.info.title, textAlign: TextAlign.left, style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 25)),
+                  Icon(
+                    widget.commandRunning == widget.info.command ? Icons.pause_circle_filled : Icons.play_circle_fill,
+                    color: Colors.white,
+                    size: 50.0,
+                  ),
+                ],
+              )
+          )
         ),
     );
   }
