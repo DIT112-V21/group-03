a31d05cf9d967ac672477632dcd18896d1ac1f35
inaj <inaj@student.chalmers.se>
2021-05-20 15:35:56 +0200

    Changed test class to check if a specific button is in bottomnavbar_widget_Test.dart.

--- smart_pet_buddy/test/bottomnavbar_widget_Test.dart
+++ smart_pet_buddy/test/bottomnavbar_widget_Test.dart
@@ -5,46 +5,33 @@ import 'package:smart_pet_buddy/bottomnavbar_widget.dart';
 
 
 void main() {
-  testWidgets("home button", (WidgetTester tester) async {
+  testWidgets("Finds a Home button", (WidgetTester tester) async {
     final homeButton = find.byIcon(Icons.home_outlined);
-
     await tester.pumpWidget(MaterialApp(home: ConvexBottomBar(Firebase.app())));
-    await tester.tap(homeButton);
-    await tester.pump();
 
     expect(homeButton, findsOneWidget);
   });
 
-  testWidgets("Play button", (WidgetTester tester) async {
+  testWidgets("Finds a Play button", (WidgetTester tester) async {
     final playButton = find.byIcon(Icons.control_camera_outlined);
-
     await tester.pumpWidget(MaterialApp(home: ConvexBottomBar(Firebase.app())));
-    await tester.tap(playButton);
-    await tester.pump();
 
     expect(playButton, findsOneWidget);
-
   });
 
-  testWidgets("Automated play button", (WidgetTester tester) async {
+  testWidgets("Finds a Automated play button", (WidgetTester tester) async {
     final autoButton = find.byIcon(Icons.pest_control_rodent_outlined);
-
     await tester.pumpWidget(MaterialApp(home: ConvexBottomBar(Firebase.app())));
-    await tester.tap(autoButton);
-    await tester.pump();
 
     expect(autoButton, findsOneWidget);
 
   });
 
-  testWidgets("Profile button", (WidgetTester tester) async {
+  testWidgets("Finds a Profile button", (WidgetTester tester) async {
     final profileButton = find.byIcon(Icons.account_circle_outlined);
-
     await tester.pumpWidget(MaterialApp(home: ConvexBottomBar(Firebase.app())));
-    await tester.tap(profileButton);
-    await tester.pump();
 
     expect(profileButton, findsOneWidget);
 
-  });
+   });
   }
