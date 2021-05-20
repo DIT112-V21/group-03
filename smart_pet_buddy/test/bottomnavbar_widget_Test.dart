9963e0872d21bc8c165f4744aee5238a9a24785c
inaj <inaj@student.chalmers.se>
2021-05-20 12:02:26 +0200

    - Added widget test for bottomnavbar_widget.dart.

---
+++ smart_pet_buddy/test/bottomnavbar_widget_Test.dart
@@ -0,0 +1,50 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:smart_pet_buddy/bottomnavbar_widget.dart';
+
+
+void main() {
+  testWidgets("home button", (WidgetTester tester) async {
+    final homeButton = find.byIcon(Icons.home_outlined);
+
+    await tester.pumpWidget(MaterialApp(home: ConvexBottomBar(Firebase.app())));
+    await tester.tap(homeButton);
+    await tester.pump();
+
+    expect(find.byIcon(Icons.home_outlined), findsOneWidget);
+  });
+
+  testWidgets("Play button", (WidgetTester tester) async {
+    final playButton = find.byIcon(Icons.control_camera_outlined);
+
+    await tester.pumpWidget(MaterialApp(home: ConvexBottomBar(Firebase.app())));
+    await tester.tap(playButton);
+    await tester.pump();
+
+    expect(find.byIcon(Icons.control_camera_outlined), findsOneWidget);
+
+  });
+
+  testWidgets("Automated play button", (WidgetTester tester) async {
+    final autoButton = find.byIcon(Icons.pest_control_rodent_outlined);
+
+    await tester.pumpWidget(MaterialApp(home: ConvexBottomBar(Firebase.app())));
+    await tester.tap(autoButton);
+    await tester.pump();
+
+    expect(find.byIcon(Icons.pest_control_rodent_outlined), findsOneWidget);
+
+  });
+
+  testWidgets("Profile button", (WidgetTester tester) async {
+    final profileButton = find.byIcon(Icons.account_circle_outlined);
+
+    await tester.pumpWidget(MaterialApp(home: ConvexBottomBar(Firebase.app())));
+    await tester.tap(profileButton);
+    await tester.pump();
+
+    expect(find.byIcon(Icons.account_circle_outlined), findsOneWidget);
+
+  });
+  }
