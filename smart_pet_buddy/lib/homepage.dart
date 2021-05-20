0ada08fc4c60a1d83f85a914f4ca74edf0499d84
Emilie Anthony <emilieanthony@live.se>
2021-05-20 18:44:22 +0200

    remove unused import

--- smart_pet_buddy/lib/homepage.dart
+++ smart_pet_buddy/lib/homepage.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:mqtt_client/mqtt_client.dart';
 import 'package:mqtt_client/mqtt_server_client.dart';
 import 'package:smart_pet_buddy/spbMqttClient.dart';
 import 'constants.dart';
