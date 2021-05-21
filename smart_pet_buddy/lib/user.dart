9da8df0431ac5b21362e204e353ea9b1a938ca72
olga <gusratol@student.gu.se>
2021-05-21 17:51:23 +0200

    new options for Profile Page and Edit Profile Page responding log out Button

--- smart_pet_buddy/lib/user.dart
+++ smart_pet_buddy/lib/user.dart
@@ -1,15 +1,15 @@
-
-class User{
-  final String imagePath;
-  final String name;
-  final String email;
-  final String about;
-
-  const User ({
-  this.imagePath,
-  this.name,
-  this.email,
-  this.about,
-
-});
-}
+//
+// class User{
+//   final String imagePath;
+//   final String name;
+//   final String email;
+//   final String about;
+//
+//   const User ({
+//   this.imagePath,
+//   this.name,
+//   this.email,
+//   this.about,
+//
+// });
+// }
--- smart_pet_buddy/lib/user_preferences.dart
+++ smart_pet_buddy/lib/user_preferences.dart
@@ -1,13 +1,13 @@
-
-import 'package:smart_pet_buddy/user.dart';
-
-class UserPreferences {
-  static const myUser = User(
-
-   imagePath: 'https://techcommunity.microsoft.com/t5/image/serverpage/image-id/217078i525F6A9EF292601F/image-size/large?v=v2&px=999',
-      name: 'Cat owner',
-      email: 'meow@mail.com',
-      about: 'Pets are the best',
-
-  );
-}
+//
+// import 'package:smart_pet_buddy/user.dart';
+//
+// class UserPreferences {
+//   static const myUser = User(
+//
+//    imagePath: 'https://techcommunity.microsoft.com/t5/image/serverpage/image-id/217078i525F6A9EF292601F/image-size/large?v=v2&px=999',
+//       name: 'Cat owner',
+//       email: 'meow@mail.com',
+//       about: 'Pets are the best',
+//
+//   );
+// }
