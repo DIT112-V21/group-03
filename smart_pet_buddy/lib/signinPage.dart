b90187816fd844b8669a51ccd2805d8c909678c6
flyf <hry87625@gmail.com>
2021-05-29 19:23:10 +0200

    update google signin UI

--- smart_pet_buddy/lib/signinPage.dart
+++ smart_pet_buddy/lib/signinPage.dart
@@ -252,29 +252,7 @@ class _OtherProvidersSignInSectionState
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              Container(
-                alignment: Alignment.center,
-                child: const Text('Sign in with Google',
-                    style: TextStyle(fontWeight: FontWeight.bold)),
-              ),
-              Container(
-                padding: const EdgeInsets.only(top: 16),
-                alignment: Alignment.center,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    ListTile(
-                      title: const Text('Google'),
-                      leading: Radio<int>(
-                        value: 3,
-                        groupValue: _selection,
-                        onChanged: _handleRadioButtonSelected,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Container(
+                            Container(
                 padding: const EdgeInsets.only(top: 16),
                 alignment: Alignment.center,
                 child: SignInButton(
